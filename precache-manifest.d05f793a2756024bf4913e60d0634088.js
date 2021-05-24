56149f8e7ca84c9e09dd7f9aadee7320f9170a7e
cernei13 <76566283+cernei13@users.noreply.github.com>
2021-05-24 12:05:27 +0300

    Updates

---
+++ precache-manifest.d05f793a2756024bf4913e60d0634088.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673643c585c768c80cd529d72d551241",
+    "url": "/myTodolist/index.html"
+  },
+  {
+    "revision": "91dcddfb80aa5ef12447",
+    "url": "/myTodolist/static/js/2.32b9d141.chunk.js"
+  },
+  {
+    "revision": "d3e11c891b3c05308918",
+    "url": "/myTodolist/static/js/main.2e29ecd4.chunk.js"
+  },
+  {
+    "revision": "a208abf2f095e743342c",
+    "url": "/myTodolist/static/js/runtime-main.6349e5a5.js"
+  }
+]);
